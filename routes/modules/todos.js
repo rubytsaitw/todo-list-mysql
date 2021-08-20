befc19950db3bebf11312a7b012cfc4df2472900
Ruby Tsai <hjtsai0307@gmail.com>
2021-08-20 17:45:03 +0800

    feat: add create todo function

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -4,6 +4,20 @@ const router = express.Router()
 const db = require('../../models')
 const Todo = db.Todo
 
+// Get new Todo template
+router.get('/new', (req, res) => {
+  return res.render('new')
+})
+
+// Post new Todo
+router.post('/', (req, res) => {
+  const UserId = req.user.id
+  const name = req.body.name
+  return Todo.create({ UserId, name })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 router.get('/:id', (req, res) => {
   const UserId = req.user.id
   const id = req.params.id
